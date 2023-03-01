569e0467bf70e3d8284c83855871dd14423e44fb
Jacky Andrazat <jackyandrazat@gmail.com>
2023-03-01 13:29:49 +0700

    Add fitur pengguna dan custom error

---
+++ src/Infrastructures/database/postgres/pool.js
@@ -0,0 +1,15 @@
+/* istanbul ignore file */
+const { Pool } = require('pg');
+
+const testConfig = {
+    host: process.env.PGHOST_TEST,
+    port: process.env.PGHOST_TEST,
+    user: process.env.PGHOST_TEST,
+    password: process.env.PGHOST_TEST,
+    database: process.env.PGHOST_TEST,
+};
+
+const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
+
+
+module.exports = pool;
